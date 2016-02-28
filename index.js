e2d65c995da1f5fd000d5beb340f80ea0a133ab8
Matt Kime <matt@mattki.me>
2016-02-27 23:24:44 -0500

    remove a comment, add dev mode. make it the default through npm start

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var fetch = require('node-fetch'),
 	FormData = require('form-data'),
 	http = require('http');
 
-//also charles proxy
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-//var url311 = 'https://www1.nyc.gov/apps/311universalintake/form.htm'
-//this is for working with charles proxy
-var url311 = 'https://localhost:60103/apps/311universalintake/form.htm',
+var url311 = 'https://www1.nyc.gov/apps/311universalintake/form.htm',
 	userAgent= 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36';
+
+if( process.env.NODE_ENV == 'dev'){
+	//for working with charles proxy
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+	url311 = 'https://localhost:60103/apps/311universalintake/form.htm';
+}
 
 var objToFormData = ( obj ) => {
 	let data = new FormData();
@@ -50,23 +51,8 @@ var reqCookie = '';
 
 var processSecondResponse = function( res ){
 	console.log(res.status);
-		console.log( "second item" );
-		//console.log( body );
-		console.log( 'done first meaningless form' );
-		/*
-	res.text().then( body => {
-		console.log( "second item" );
-		//console.log( body );
-		console.log( 'done first meaningless form' );
-		/*
-		fetch( url311, {
-			method: 'post',
-			headers: {
-				'Cookie': reqCookie,
-			}
-		});
-	})
-	*/
+	console.log( "second item" );
+	console.log( 'done first meaningless form' );
 	return res.text();
 };
 
