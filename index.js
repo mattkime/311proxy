43b1c9d2cc86f2a34b53c54958438b681a4ee4d9
Matt Kime <matt@mattki.me>
2016-07-15 18:13:15 -0400

    remove data

--- index.js
+++ index.js
@@ -1,222 +1,15 @@
-var fetch = require('node-fetch'),
-	cookie = require('cookie'),
-	FormData = require('form-data'),
-	http = require('http'),
-	jsdom = require('jsdom'),
-	Rx = require('rxjs'),
-	beautify = require('js-beautify').html;
+var reporter = require('./lib/reporter'),
+	inquirer = require('inquirer'),
+	complaint_info = require('./lib/complaint-dsl.js');
 
 require('source-map-support').install();
-/*
-if( process.env.NODE_ENV == 'dev'){
-	//for working with charles proxy
-	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-	url311 = 'https://localhost:60103/apps/311universalintake/form.htm';
-}
-*/
-var objToFormData = ( obj ) => {
-	let data = new FormData();
 
-	Object.keys( obj ).forEach( k => data.append( k, obj[k] ) );
-	return data;
-};
-
-var startFormData = objToFormData({
-	'formFields.X_CHAR1_1': '__No',
-	'formFields.Vehicle Type': '__Yellow',
-	'formFields.Affidavit': '__Yes',
-	'formFields.Hearing': '__Yes',
-	'_target1': 'START'
-});
-
-var thirdFormData = objToFormData({
-	'formFields.Complaint Type' : '1-6VL-135',
-	'formFields.Descriptor 1' : '1-6VN-327',
-	'formFields.Descriptor 2': '1-6VO-1637',
-	'formFields.Taxi Driver Name': '',
-	'formFields.Taxi License Number':'',
-	'formFields.Taxi Medallion Number':'5J18',
-	'formFields.Complaint Details': 'taxi parked in bike lane',
-	'formFields.Date/Time of Occurrence':'02/19/2016 09:50:29 AM',
-	'_target2':''
-});
-
-//todo - create functions that compose fetches and return promises
-// create declarative language for speaking with server.
-/*
-funnction startRequest(){
-
-}
-*/
-
-var reqCookie = '';
-
-var processSecondResponse = function( res ){
-	console.log(res.status);
-	console.log( "second item" );
-	console.log( 'done first meaningless form' );
-	return res.text();
-};
-
-var secondRequest = function(){
-	return fetch311( startFormData );
-};
-
-var thirdRequest = function(){
-	return fetch311( thirdFormData );
-};
-
-
-/*
-function sendData(){
-	//todo return errors
-	newSession()
-		.then( setSessionId )
-		.then( secondRequest )
-		.then( processSecondResponse )
-		.then( thirdRequest )
-		.then( processSecondResponse )
-};
-*/
-//sendData();
-
-function reporter(){
-if( process.env.NODE_ENV == 'dev'){
-	//for working with charles proxy
-	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-	url311 = 'https://localhost:60103/apps/311universalintake/form.htm';
-}
-	var url311 = 'https://www1.nyc.gov/apps/311universalintake/form.htm',
-		userAgent= 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36';
-
-	let fetch311 = function( formData, url = url311){
-		let config = {
-			method: 'post',
-			headers: {
-				'Cookie': reqCookie,
-				'user-agent': userAgent
-			}
-		};
-
-	//only a convenince for single arg with url call
-	if( typeof formData === 'string' ){
-		url = formData;
-		formData = null;
-	}
-
-	if( formData ){
-		config.body = formData;
-		config.headers['Content-Length'] = formData.getLengthSync();
-	}
-
-	console.log( url );
-	console.log( config );
-	return fetch( url, config);
-};
-	let newSession = function(){
-		let promise = fetch311( url311 + '?serviceName=TLC%20Taxi%20Driver%20Unsafe%20Driving%20Non-Passenger');
-		return Rx.Observable.fromPromise(promise).flatMap(setSessionId);
-	};
-
-	var setSessionId = res => {
-		let cookies = res.headers.get('set-cookie'),
-			{ JSESSIONID } = cookie.parse(cookies);
-
-		if( res.status != 200 ){ console.log('ERROR'); }
-
-		reqCookie = cookie.serialize('JSESSIONID',JSESSIONID);
-
-		console.log( 'session id:', JSESSIONID );
-
-		return Rx.Observable.fromPromise( res.text() )
-			.map(function(value){
-				//nextPage
-				let document = jsdom.jsdom( value ),
-					formId = document.querySelectorAll("#formId"),
-					formIdExists = formId.length === 1,
-					nextPage = document.querySelectorAll("#nextPage"),
-					nextPageExists = false;
-
-				if( nextPage
-					&& nextPage.length === 1
-					&& nextPage[0].getAttribute("name") === "_target1" ){
-						nextPageExists = true;
-				}
-				console.log("formId present:", formId.length === 1 );
-				console.log("#nextPage:", nextPage[0].getAttribute("name"));
-				return formIdExists && nextPageExists ? JSESSIONID : null;
-			});
-	};
-
-	return {
-		sessionId : 12345,
-		page : 0,/*
-		justTheForm : function( text ){
-			let document = jsdom.jsdom( value ),
-				formId = document.querySelectorAll("#formId");
-			return formId.outerHTML;
-		},*/
-		sendData : function(data){
-		let justTheForm =  function( text ){
-			let document = jsdom.jsdom( text ),
-				formId = document.querySelectorAll("#formId"),
-				formHtml = formId[0].outerHTML;
-
-			return require('js-beautify').html( formHtml.replace(/\n/g, ""), { "preserve_newlines": true, "indent_size": 0 } );
-		};
-			let that = this;
-			return Rx.Observable.create(function(ob){
-				var i = 0;
-				var getUrl = function(data){
-					//todo set url, convert to formdata....set cookie?
-					return Rx.Observable.fromPromise( fetch311( objToFormData( data ), url311 + '?serviceName=TLC%20Taxi%20Driver%20Unsafe%20Driving%20Non-Passenger'))
-						.flatMap( res => Rx.Observable.fromPromise(res.text()) ).do( val => console.log( justTheForm(val).replace(/\n/g, "")) )
-					};
-
-				var sub = getUrl(data[i++]);
-				sub.subscribe(subscribe);
-
-				function subscribe(x) {
-						console.log('Next: ' + x.length);
-						ob.next(x);
-						if(i < data.length){
-						  getUrl(data[i++]).subscribe(subscribe);
-						}else{
-						  ob.complete();
-						}
-				}
-			});
-		},
-		getSession : function(){
-			return Rx.Observable.create(function (ob) {
-				let that = this;
-					//this should set sessionId or return error. no need to return value
-					//observ = newSession.call(this)
-					newSession.call(this)
-						.map( val => {
-							this.sessionId = val;
-							return val;
-						}).subscribe(function(val){
-							let data$ = Rx.Observable.create(function (ob) {
-								data.forEach(function(item){
-
-								});
-
-							})
-							console.log("inner subscribe");
-							console.log(val);
-							ob.next( val );
-							ob.complete();
-						});
-				//return observ;
-			});
-		},
-		report : function(data){
-			var sendData = this.sendData;
-			return this.getSession().flatMap(() => sendData(data));
-		}
-	};
-}
+inquirer.prompt([
+	{'name':'complaint_type',
+	'message': 'complaint type',
+	'type':'list',
+	choices: complaint_info.map( item => item.name )}]).then( answers => 
+	console.log(answers));
 
 var data = [{
 		'formFields.X_CHAR1_1': '__No',
@@ -252,7 +45,43 @@ var data = [{
 		'formFields.Contact Street Name':'',
 		'formFields.Contact Apartment Number':'',
 		'_target4':''
-	}*/
+	} */
 	];
 
-reporter().report(data).subscribe((val) => console.log('hi'))
+//reporter().report('TLC%20Taxi%20Driver%20Unsafe%20Driving%20Non-Passenger', data).subscribe((val) => console.log('hi'))
+//
+var data = [{
+	'_target1' : 'START'
+	}, {
+		'formFields.Complaint Type': '1-6VL-207',
+		'formFields.Descriptor 1': '1-6VN-6', //from dropdown
+		'formFields.Complaint Details': 'pickup truck parked in \'never park here\' zone in front of rei on lafayette st',
+		'formFields.Date/Time of Occurrence': '06/30/2016 09:36:12 AM',
+		'formFields.ATTRIB_46': 'this is a daily problem',
+		'_target2':''
+	}, {
+		'formFields.Location Type': '1-6VO-1630',
+		'formFields.Address Type': '__Street',
+		'formFields.Incident Borough 6': '1-4X9-316',
+		'formFields.Incident Address Number': '303',
+		'formFields.Incident Street Name': 'Lafayette St',
+		'formFields.Location Details': '',
+		'_target3': ''
+	}, {
+		'formFields.Personal Email Address': 'matt@mattki.me',
+		'formFields.Contact First Name': 'Matt',
+		'formFields.Contact Last Name': 'Kime',
+		'formFields.Contact Business Phone': '616-666-6561',
+		'formFields.Contact Home Phone': '616-666-6561',
+		'formFields.Contact Borough': '1-4X9-314',
+		'formFields.Contact Address Number': '167',
+		'formFields.Contact Street Name': 'Quincy St.',
+		'formFields.Contact Apartment Number': '2',
+		'_target4' : ''
+	}, {
+		'replace_target': '',
+		'_finish': 'SUBMIT FORM'
+	}
+	];
+
+//reporter().report('NYPD+Parking', data).subscribe((val) => console.log('hi'))
