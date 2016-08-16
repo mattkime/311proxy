dc994617833a620023398f5f0d0ed9384f9931fb
Matt Kime <matt@mattki.me>
2016-08-16 17:22:55 -0400

    sketch out prompt

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ var reporter = require('./lib/reporter'),
 	config = require('./config.json');
 
 require('source-map-support').install();
-/*
-inquirer.prompt([
-	{'name':'complaint_type',
-	'message': 'complaint type',
-	'type':'list',
-	choices: complaint_info.map( item => item.name )}]).then( answers => 
-	console.log(answers));
-*/
 
+inquirer.prompt([
+	{'name':'plate',
+	'message': 'license plate'},
+	{'name':'complaint',
+	'message':'complaint'},
+	{'name':'date_time',
+	'message':'Incident date/time'},
+	{'name':'location_type',
+	'message':'location type'},
+	{'name':'on_street',
+	'message':'on street'},
+	{'name':'cross_street',
+	'message':'cross street'},
+	{'name':'location_details',
+	'message':'location details'},
+	{'name':'photo',
+	'message':'photo'},
+	{'name':'photo_2',
+	'message':'photo 2'}]).then( answers =>
+		console.log(answers)
+		#reporter().report('TLC%20FHV%20Driver%20Unsafe%20Driving', formatTaxiComplaint( answers) ).subscribe(() => {})
+	);
+
+/*
 var yelowData = [{
 		'formFields.X_CHAR1_1': '__No',
 		'formFields.Vehicle Type': '__Yellow',
@@ -49,43 +65,46 @@ var yelowData = [{
 		'_target4':''
 	} 
 	];
+*/
 
-var carserviceData = [{
-		'formFields.X_CHAR1_1':'__Within the 5 Boroughs of New York City',
-		'formFields.License Type':'1-B3X-3', //Vehicle License Plate
-		'formFields.Affidavit': '__Yes',
-		'formFields.Hearing': '__Yes',
-		'_target1': 'START',
-	}, {
-		'formFields.Complaint Type' : '1-B3X-15',
-		'formFields.Descriptor 1' : '1-B3Y-2', //i have no idea what these fields mean
-		'formFields.Descriptor 2': '1-B3Z-6',
-		'formFields.Taxi License Number': '',
-		'formFields.Vehicle Type' : '1-B3X-7',
-		'formFields.Complaint Details': 'cab blocking bike lane',
-		'formFields.Date/Time of Occurrence':'06/19/2016 10:02:00 AM',
-		'_target2':'',
-	},{
-		'formFields.Location Type': 'Street',
-		'formFields.Address Type': '__Intersection',
-		'formFields.Incident Borough 5': '1-2ZR', // bronx - 1-2ZN, brooklyn - 1-2ZP, manhattan - 1-2ZR, queens - 1-2ZT, staten island - 1-2ZV// bronx 1-4X9-313, brooklyn 1-4X9-314, manhattan 1-4X9-316, queens 1-4X9-315, staten island 1-4X9-318
-		'formFields.On Street':'Chrystie St',
-		'formFields.Cross Street 1': 'Hester  St',
-		'formFields.Location Details':'',
-		'_target3':'',
-	}, {
-		...config.contact_info,
-		'_target4':''
-	}, {
-		'replace_target': '',
-		'file1': require('fs').createReadStream(''),
-		'_finish': 'SUBMIT FORM'
-	}
-	];
+var formatTaxiComplaint( fields ){
+	var carserviceData = [{
+			'formFields.X_CHAR1_1':'__Within the 5 Boroughs of New York City',
+			'formFields.License Type':'1-B3X-3', //Vehicle License Plate
+			'formFields.Affidavit': '__Yes',
+			'formFields.Hearing': '__Yes',
+			'_target1': 'START',
+		}, {
+			'formFields.Complaint Type' : '1-B3X-15',
+			'formFields.Descriptor 1' : '1-B3Y-2', //i have no idea what these fields mean
+			'formFields.Descriptor 2': '1-B3Z-6',
+			'formFields.Taxi License Number': '',
+			'formFields.Vehicle Type' : '1-B3X-7',
+			'formFields.Complaint Details': 'cab blocking bike lane',
+			'formFields.Date/Time of Occurrence':'06/19/2016 10:02:00 AM',
+			'_target2':'',
+		},{
+			'formFields.Location Type': 'Street',
+			'formFields.Address Type': '__Intersection',
+			'formFields.Incident Borough 5': '1-2ZR', // bronx - 1-2ZN, brooklyn - 1-2ZP, manhattan - 1-2ZR, queens - 1-2ZT, staten island - 1-2ZV// bronx 1-4X9-313, brooklyn 1-4X9-314, manhattan 1-4X9-316, queens 1-4X9-315, staten island 1-4X9-318
+			'formFields.On Street':'Chrystie St',
+			'formFields.Cross Street 1': 'Hester  St',
+			'formFields.Location Details':'',
+			'_target3':'',
+		}, {
+			...config.contact_info,
+			'_target4':''
+		}, {
+			'replace_target': '',
+			//'file1': require('fs').createReadStream(''),
+			'_finish': 'SUBMIT FORM'
+		}];
+
+	return carserviceData
+};
 
 //reporter().report('TLC%20Taxi%20Driver%20Unsafe%20Driving%20Non-Passenger', data).subscribe((val) => console.log('hi'))
 //todo - print val, return document.queryElementsById('#sr_number').innerHtml
-reporter().report('TLC%20FHV%20Driver%20Unsafe%20Driving', carserviceData).subscribe(() => {})
 //
 var data = [{
 	'_target1' : 'START'
